e2f3e16900ff1991e82a4e3347300775ef4947d0
h1dd3n01 <h1dd3n1@protonmail.com>
2020-05-20 18:41:17 +0300

    final touch

--- hunter/pipelines.py
+++ hunter/pipelines.py
@@ -37,38 +37,3 @@ class HunterPipeline(object):
             print('File was opened')
             f.write(json.dumps(self.json_data))
             f.close()
-
-
-    # def process_item(self, item, spider):
-    #
-    #     flag = False
-    #     if os.path.isfile('scrapedData.json'):
-    #         with open('scrapedData.json', 'r+') as f:
-    #             file = f.read()
-    #             json_file = json.loads(file)
-    #             for i in json_file['main']:
-    #                 if item['main_item']['title'] in i['title']:
-    #                     i['lead'].append(item['secondary_item'])
-    #                     # json_file['main'].append(i)
-    #                     f.seek(0)
-    #                     f.write(json.dumps(json_file))
-    #                     f.close()
-    #                     flag = True
-    #                     break
-    #             if not flag:
-    #                 item['main_item']['lead'] = []
-    #                 item['main_item']['lead'].append(item['secondary_item'])
-    #                 json_file['main'].append(item['main_item'])
-    #                 f.seek(0)
-    #                 f.write(json.dumps(json_file))
-    #                 f.close()
-    #
-    #     else:
-    #         with open('scrapedData.json', 'a+') as f:
-    #             json_file = {'main': []}
-    #             item['main_item']['lead'] = []
-    #             item['main_item']['lead'].append(item['secondary_item'])
-    #             json_file['main'].append(item['main_item'])
-    #             f.write(json.dumps(json_file))
-    #             f.close()
-    #     return item
